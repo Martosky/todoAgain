dd45407b9cc2a6f62b6c7b7d4387e7c1af04bf3f
Martosky <imoniaromartins@yahoo.com>
2024-11-12 21:20:11 +0100

    working on the program

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-const inputCont = document.getElementById("text-input");
+const inputContent = document.getElementById("text-input");
 const output = document.getElementById("output");
 const button = document.getElementById("button");
+
+button.addEventListener("click", addItemFunc)
+
+function addItem(){
+    if(inputContent.value === ""){
+    alert("Put a valid input")
+ }else{
+    let li = document.createElement("li");
+    li.innerHTML = inputContent.value;
+    output.appendChild(li)
+ }
+}

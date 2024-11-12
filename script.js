21c719319fadc6994c4cf5debbdad149a2591f7a
Martosky <imoniaromartins@yahoo.com>
2024-11-12 22:37:16 +0100

    done for now

--- script.js
+++ script.js
@@ -18,3 +18,12 @@ function addItemFunc(){
  inputContent.value = ""
  saveData()
 }
+
+output.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked")
+        saveData()
+    }else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove()
+    }
+})
